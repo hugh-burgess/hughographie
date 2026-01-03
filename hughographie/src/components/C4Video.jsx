e4e4700c9eb002e5c86b7ddfd3bfdaed0925f68f
hughburgessgermany@hotmail.com <hughburgessgermany@hotmail.com>
2026-01-03 21:47:28 +0100

    video

--- hughographie/src/components/C4Video.jsx
+++ hughographie/src/components/C4Video.jsx
@@ -2,22 +2,23 @@ import processedImageUrl from "../utils/imageOptimization";
 
 const C4Video = ({ blok }) => {
     const imageProps = processedImageUrl(blok.placeholder.filename);
+    if (!blok.source.id) return null
     return <div className="video">
-        {/* {blok.source.id &&
-            <video
-                className="source"
-                src={blok.source.filename}
-                controls
-                autoPlay
-                muted
-                loop
-            />} */}
-        {blok.placeholder && <img
+        <video
+            className="source"
+            src={blok.source.filename}
+            controls
+            autoPlay
+            muted
+            loop
+        />
+        {blok.placeholder.filename && <img
             {...imageProps}
             className="placeholder" src={blok.placeholder.filename} alt="Placeholder" />}
         {blok.caption &&
             <caption>© {blok.caption}</caption>}
     </div>
+
 }
 
 export default C4Video;
