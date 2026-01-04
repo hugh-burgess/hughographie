89eb7a88997062677b44e8cd3048ad0b4467b85d
hughburgessgermany@hotmail.com <hughburgessgermany@hotmail.com>
2026-01-04 20:21:06 +0100

    stage - remove video

--- hughographie/src/components/C5Stage.jsx
+++ hughographie/src/components/C5Stage.jsx
@@ -1,11 +1,9 @@
 import processedImageUrl from "../utils/imageOptimization";
-import C4Video from "./C4Video";
 
 const C5Stage = ({ blok }) => {
         const imageProps = processedImageUrl(blok.image.filename);
     return <>
         <section className="stage">
-            {blok.video[0] && <C4Video blok={blok.video[0]} />}
             {blok.image && <img
             {...imageProps}
             className={`image ${blok.imagePosition || ''}`} src={blok.image.filename} alt="Stage" />}
