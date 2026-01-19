4029fe4d4ad4604ea2d22e545a10a0525242b592
hughburgessgermany@hotmail.com <hughburgessgermany@hotmail.com>
2026-01-19 13:42:44 +0100

    stage image rejig

--- hughographie/src/components/C5Stage.jsx
+++ hughographie/src/components/C5Stage.jsx
@@ -1,11 +1,12 @@
 import Image from "../globals/Image";
 
 const C5Stage = ({ blok }) => {
+    const item = blok.image[0]
     return <>
         <section className="stage">
-            {blok.image && <Image
-                image={blok.image}
-                className={`image ${blok.imagePosition || ''}`} />
+            {item && <Image
+                image={item.image}
+                className={`image ${item.imagePosition || ''}`} />
             }
         </section>
     </>
