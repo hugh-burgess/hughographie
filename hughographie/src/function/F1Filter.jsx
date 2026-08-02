c0032b42905dfa29fe692b0879b4350a578362e2
hughburgessgermany@hotmail.com <hughburgessgermany@hotmail.com>
2026-08-02 12:23:11 +0200

    F1Filter - mobile mode

--- hughographie/src/function/F1Filter.jsx
+++ hughographie/src/function/F1Filter.jsx
@@ -3,6 +3,7 @@ import Button from "../elements/Button";
 
 const F1Filter = ({ arrayToFilter, filterTarget, children }) => {
     const [activeFilter, setActiveFilter] = useState(null);
+    const [showMobileFilters, setShowMobileFilters] = useState(false)
 
     const filters = [
         ...new Set(arrayToFilter?.map(item => item.content[filterTarget]))
@@ -14,22 +15,37 @@ const F1Filter = ({ arrayToFilter, filterTarget, children }) => {
         )
         : arrayToFilter;
 
-    return (
-        <>
-            <div className="f1-filter">
-                <Button onClick={() => setActiveFilter(null)} className={`${activeFilter === null ? 'is-active' : ''}`}>
+    const Filters = ({ className }) => {
+        return (
+            <>
+                <Button onClick={() => setActiveFilter(null)} className={`${className} ${activeFilter === null ? 'is-active' : ''}`}>
                     All
                 </Button>
-
                 {filters.map((filter, index) => (
                     <Button
                         key={filter}
                         onClick={() => setActiveFilter(filter)}
-                        className={`${activeFilter === filter ? 'is-active' : ''}`}
+                        className={`${className} ${activeFilter === filter ? 'is-active' : ''}`}
                     >
                         {filter}
                     </Button>
                 ))}
+            </>
+        )
+    }
+
+    return (
+        <>
+            <div className="f1-filter">
+                <div className="desktop-filters">
+                    <Button className={`mobile-filter-button ${showMobileFilters ? 'active' : ''}`} onClick={() => setShowMobileFilters(!showMobileFilters)}>
+                        Filters
+                    </Button>
+                    <Filters />
+                </div>
+                <div className={`mobile-filters ${showMobileFilters ? 'visible' : 'hidden'}`}>
+                    <Filters className="mobile-filter" />
+                </div>
             </div>
 
             {children(filteredItems)}
