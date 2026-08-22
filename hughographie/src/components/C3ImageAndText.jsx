642e1a07e0deb4749989e014548479a2cda4cf60
hughburgessgermany@hotmail.com <hughburgessgermany@hotmail.com>
2026-08-22 23:37:16 +0200

    richtext and image: a third styles on image

--- hughographie/src/components/C3ImageAndText.jsx
+++ hughographie/src/components/C3ImageAndText.jsx
@@ -2,12 +2,12 @@ import Image from "../elements/Image";
 import C2RichText from "./C2RichText";
 
 const C3ImageAndText = ({ fields }) => {
-    const { variant, image, content } = fields;
+    const { variant, image, content, isImageAThird } = fields;
     const imageFirst = variant === 'imageText';
     const availableText = content.content[0].content
     const imageItem = image[0]
     return (
-        <section className={`image-and-text-grid ${imageFirst ? '' : 'reverse'}`}>
+        <section className={`image-and-text-grid ${imageFirst ? '' : 'reverse'} ${isImageAThird ? 'image-third' : ''}`}>
             {imageItem && <Image item={imageItem} containerClassName="image"
             />}
             {availableText && <C2RichText className="text" content={content} />}
